3256478cd6cf3d2bb22fe4e2f8cec7521f8d9d30
brandoncjh <brandonchongjh@gmail.com>
2020-02-08 15:14:11 +0800

    Updates

---
+++ precache-manifest.576430bfd5e89f202b2e8cb9978fe92e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3dd5e466c231bafa947a8ce1c3e30df",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "190c41214d85c83a9834",
+    "url": "/weatherapp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d4715a092ddd7384129a",
+    "url": "/weatherapp/static/css/main.6d06fa46.chunk.css"
+  },
+  {
+    "revision": "190c41214d85c83a9834",
+    "url": "/weatherapp/static/js/2.d2203df8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weatherapp/static/js/2.d2203df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4715a092ddd7384129a",
+    "url": "/weatherapp/static/js/main.9070886a.chunk.js"
+  },
+  {
+    "revision": "39d82609d2bde4da64bb",
+    "url": "/weatherapp/static/js/runtime-main.f0eb2130.js"
+  }
+]);
